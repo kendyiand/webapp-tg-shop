4b59556867d35604f7da06df4662bdf1c93fccf3
Max <hellmax2206@gmail.com>
2023-11-09 00:30:32 +0300

    add urls and views to product page

--- ecom/ecom/store/views.py
+++ ecom/ecom/store/views.py
@@ -4,4 +4,9 @@ from .models import Product
 
 def home(request):
     products = Product.objects.all()
-    return render(request, 'home.html', {'products':products})
+    return render(request, 'home.html', {'products': products})
+
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
